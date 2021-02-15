1b9b4d31adf3b35115c6f0f5d0f73f4f98eda2bc
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-15 11:13:09 +0300

    add elements for tracking typing

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -1,14 +1,73 @@
-import React from 'react'
-import { Paper } from '@material-ui/core'
+import React from 'react';
+import {
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Paper, Divider
+} from '@material-ui/core';
 
-const SidePanel = ({paperStyle}) => {
-
-
+const SidePanel = ({ paperStyle }) => {
     return (
-        <Paper style={paperStyle}>
-            This is pannel
+        <Paper style={paperStyle, {flexDirection: 'row'}}>
+            <List>
+                <ListItem>
+                    <ListItemIcon style={iconStyleWrapper}>
+                        <i
+                            style={{
+                                fontSize: '25px',
+                                color: 'rgb(93, 173, 226)',
+                            }}
+                            class='fas fa-tachometer-alt'></i>
+                    </ListItemIcon>
+                    <ListItemText style={textStyle}>100 слов/мин</ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon style={iconStyleWrapper}>
+                        <i
+                            style={{
+                                fontSize: '29px',
+                                color: 'rgb(  175, 122, 197 )',
+                            }}
+                            class='fas fa-crosshairs'></i>
+                    </ListItemIcon>
+                    <ListItemText style={textStyle}>95%</ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon style={iconStyleWrapper}>
+                        <i
+                            style={{
+                                fontSize: '30px',
+                                color: 'rgb( 236, 112, 99)',
+                            }}
+                            class='fas fa-times'></i>
+                    </ListItemIcon>
+                    <ListItemText style={textStyle}>ошибки: 2</ListItemText>
+                </ListItem>
+                </List>
+                <Divider />
+                <List>
+                <ListItem style={{ cursor: 'pointer' }}>
+                    <ListItemIcon style={iconStyleWrapper}>
+                        <i
+                            style={{
+                                fontSize: '22px',
+                                color: 'rgb(  88, 214, 141 )',
+                            }}
+                            class='fas fa-redo-alt'></i>
+                    </ListItemIcon>
+                    <ListItemText style={textStyle}>Заново</ListItemText>
+                </ListItem>
+                </List>
         </Paper>
-    )
-}
+    );
+};
 
-export default SidePanel
+const textStyle = {
+    color: 'rgb(  123, 125, 125  )',
+};
+
+const iconStyleWrapper = {
+    justifyContent: 'center',
+};
+export default SidePanel;

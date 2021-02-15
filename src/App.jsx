e6f72422b5e534b9fb88337c9b384f5ba0685743
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-15 09:28:33 +0300

    add material ui wrapper and loading to this component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './style.css'
+
+import { Container, Grid, LinearProgress } from '@material-ui/core';
+
 import TextTyping from './components/TextTyping';
+import SidePanel from './components/SidePanel';
 
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import { getData } from './action/action';
 
-import store from './store';
 
-const App = () => {
+const App = ({ loading, getData }) => {
+    useEffect(() => {
+        getData();
+        // eslint-disable-next-line
+    }, []);
+
+    if(loading ){
+        return <LinearProgress />
+    }
     return (
-        <Provider store={store}>
-            <div className='container'>
-                <TextTyping />
-            </div>
-        </Provider>
+        <Container maxWidth='xl'>
+            <Grid spacing={4} container>
+                <Grid item xs={9}><TextTyping paperStyle={paperStyle}/></Grid>
+                <Grid style={{justifyContent: 'center'}} item xs={3}><SidePanel paperStyle={paperStyle} /></Grid>
+            </Grid>
+        </Container>
     );
 };
 
-export default App;
+const paperStyle = {
+    padding: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    textAlign: 'justify'
+}
+
+const mapStateToProps = (state) => ({
+    loading: state.loading,
+});
+
+export default connect(mapStateToProps, {getData})(App);

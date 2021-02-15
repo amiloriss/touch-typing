3be917741bb807256c860e6a0ece0e01562fd49c
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-15 09:29:31 +0300

    add panel for track typing

---
+++ src/components/SidePanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Paper } from '@material-ui/core'
+
+const SidePanel = ({paperStyle}) => {
+
+
+    return (
+        <Paper style={paperStyle}>
+            This is pannel
+        </Paper>
+    )
+}
+
+export default SidePanel

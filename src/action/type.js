4bd081703a0bb310089f4760e56f9d975c6c88e5
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-16 14:52:57 +0300

    add new types for tracking typing

--- src/action/type.js
+++ src/action/type.js
@@ -1,2 +1,6 @@
 export const GET_DATA = 'GET_DATA';
 export const GET_ERROR = 'GET_ERROR';
+export const UPDATE_SPEED = 'UPDATE_SPEED';
+export const UPDATE_ACCURACY = 'UPDATE_ACCURACY';
+export const UPDATE_MISTAKE = 'UPDATE_MISTAKE';
+export const UPDATE_COUNT = 'UPDATE_COUNT';

d52715203ff229cca7647d62a116e4ffccba7fee
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-16 14:53:24 +0300

    create new actions for tracking typing

--- src/action/action.js
+++ src/action/action.js
@@ -1,16 +1,50 @@
 import axios from 'axios';
-import { GET_DATA, GET_ERROR } from './type';
+import {
+    GET_DATA,
+    GET_ERROR,
+    UPDATE_ACCURACY,
+    UPDATE_SPEED,
+    UPDATE_MISTAKE,
+    UPDATE_COUNT,
+    NEW_START,
+} from './type';
 
 export const getData = () => async (dispatch) => {
     try {
         const res = await axios.get(
             'https://baconipsum.com/api/?type=all-meat&sentences=5'
         );
-        const data = await res.data;
+        const data = res.data
+            .toString()
+            .split('')
+            .map((letter) => {
+                return {
+                    letter,
+                    current: null,
+                    success: null,
+                    failure: null,
+                };
+            });
         dispatch({ type: GET_DATA, payload: data });
-
-        // throw Error('Something went wrong, please try later');
     } catch (err) {
         dispatch({ type: GET_ERROR, payload: err });
     }
 };
+
+export const updateSpeed = (speedCount) => ({
+    type: UPDATE_SPEED,
+    payload: speedCount,
+});
+export const updateAccuracy = (accuracyCount) => ({
+    type: UPDATE_ACCURACY,
+    payload: accuracyCount,
+});
+export const updateMistake = (mistakeCount) => ({
+    type: UPDATE_MISTAKE,
+    payload: mistakeCount,
+});
+
+export const setCount = (count) => ({
+    type: UPDATE_COUNT,
+    payload: count,
+});

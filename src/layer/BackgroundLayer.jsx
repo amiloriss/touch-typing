79606a2963af7b879b1d7927351e5592b1c9ed77
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-02-17 14:29:31 +0300

    add background layer

---
+++ src/layer/BackgroundLayer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BackgroundLayer = props => {
+	return <div style={backgroundStyle}>{props.children}</div>;
+};
+
+const backgroundStyle = {
+	top: 0,
+	left: 0,
+	position: 'absolute',
+	width: '100vw',
+	height: '100vh',
+	backgroundColor: 'rgba(0,0,0,0.1)',
+	zIndex: 2,
+};
+
+export default BackgroundLayer;

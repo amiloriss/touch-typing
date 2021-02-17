e4f21be4d96542cd2ab918e2d7901e8df33b2d62
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-02-17 11:55:21 +0300

    think up to end of typing logic

---
+++ src/components/ModalWindow.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const ModalWindow = () => {
+	return <div></div>;
+};
+
+const mapStateToProps = () => ({});
+export default connect(mapStateToProps)(ModalWindow);
--- src/components/TextTyping.jsx
+++ src/components/TextTyping.jsx
@@ -1,61 +1,83 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Paper } from '@material-ui/core';
 
 import { updateMistake, setCount } from '../action/action';
 
-const TextTyping = ({ paperStyle, text, mistake, updateMistake, setCount, count }) => {
-    let index = 0;
-    text[count].current = true;
-    useEffect(() => {
-        document.body.addEventListener('keydown', (e) => {
-            if (
-                e.keyCode !== 16 &&
-                e.keyCode !== 20 &&
-                e.key.match(/^[a-zA-Z0-9_' '_._,-]*$/)
-            ) {
-                text[count].current = null;
+const TextTyping = ({
+	paperStyle,
+	text,
+	mistake,
+	updateMistake,
+	setCount,
+	count,
+}) => {
+	let index = 0;
+	if (count < text.length) {
+		text[count].current = true;
+	}
+	console.log(text.length);
+	useEffect(() => {
+		document.body.addEventListener('keydown', e => {
+			console.log(text[count]);
+			if (
+				e.keyCode !== 16 &&
+				e.keyCode !== 20 &&
+				e.key.match(/^[a-zA-Z0-9_' '_._,-]*$/)
+			) {
+				if (count < text.length) {
+					text[count].current = null;
+				}
 
-                if (e.key === text[count].letter) {
-                    text[count].success = true;
-                    setCount((count += 1));
-                } else {
-                    text[count].success = null;
-                    text[count].failure = true;
-                    setCount((count += 1));
-                    updateMistake((mistake += 1));
-                }
-            }
-        });
-    }, [text]);
+				if (count < text.length) {
+					if (e.key === text[count].letter) {
+						text[count].success = true;
+						setCount((count += 1));
+					} else {
+						text[count].success = null;
+						text[count].failure = true;
+						setCount((count += 1));
+						updateMistake((mistake += 1));
+					}
+					if (count === text.length) {
+						// here i will call modal window
+						alert('nice');
+					}
+				}
+			}
+		});
+	}, [text]);
 
-    return (
-        <Paper style={paperStyle}>
-            <div style={{ fontSize: '22px', lineHeight: '1.4' }}>
-                {text.map((el) => {
-                    index++;
-                    return (
-                        <span
-                            style={{ padding: '0 3px', margin: '0 1px' }}
-                            key={index}
-                            className={
-                                el.current
-                                    ? 'current-letter'
-                                    : el.success
-                                    ? 'passed'
-                                    : el.failure && 'failure'
-                            }>
-                            {el.letter}
-                        </span>
-                    );
-                })}
-            </div>
-        </Paper>
-    );
+	return (
+		<Paper style={paperStyle}>
+			<div style={{ fontSize: '22px', lineHeight: '1.4' }}>
+				{text.map(el => {
+					index++;
+					return (
+						<span
+							style={{ padding: '0 3px', margin: '0 1px' }}
+							key={index}
+							className={
+								el.current
+									? 'current-letter'
+									: el.success
+									? 'passed'
+									: el.failure && 'failure'
+							}
+						>
+							{el.letter}
+						</span>
+					);
+				})}
+			</div>
+		</Paper>
+	);
 };
 
-const mapStateToProps = (state) => {
-    return { text: state.data, mistake: state.typingMistake, count: state.count };
+const mapStateToProps = state => {
+	return { text: state.data, mistake: state.typingMistake, count: state.count };
 };
 
-export default connect(mapStateToProps, { updateMistake, setCount })(TextTyping);
+export default connect(mapStateToProps, { updateMistake, setCount })(
+	TextTyping
+);

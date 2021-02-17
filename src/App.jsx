298254d794f610a398d64885f94547206d249c8e
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-02-17 14:30:20 +0300

    modal window in app and some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,73 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 import {
-    AppBar,
-    Container,
-    Grid,
-    LinearProgress,
+	AppBar,
+	Container,
+	Grid,
+	LinearProgress,
+	Paper,
 } from '@material-ui/core';
 
 import TextTyping from './components/TextTyping';
-import SidePanel from './components/SidePanel';
+import ResultPanel from './components/ResultPanel';
 
 import { connect } from 'react-redux';
 import { getData } from './action/action';
+import ModalWindow from './components/ModalWindow';
 
 const App = ({ loading, getData, data }) => {
-    useEffect(() => {
-        getData();
-        // eslint-disable-next-line
-    }, []);
+	const [isCompleted, setIsCompleted] = useState(false);
+	useEffect(() => {
+		getData();
+		// eslint-disable-next-line
+	}, []);
 
-    if (loading || data === null) {
-        return <LinearProgress />;
-    }
-    return (
-        <>
-            <AppBar position='relative' style={{ padding: '20px 35px' }}>
-                <h2>Touch Typing Speed</h2>
-            </AppBar>
-            <Container style={{ marginTop: '20px' }} maxWidth='xl'>
-                <Grid spacing={4} container>
-                    <Grid item xs={9}>
-                        <TextTyping paperStyle={paperStyle} />
-                    </Grid>
-                    <Grid
-                        style={{ justifyContent: 'center', minWidth: '240px' }}
-                        item
-                        xs={3}>
-                        <SidePanel paperStyle={paperStyle} />
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    );
+	if (loading || data === null) {
+		return <LinearProgress />;
+	}
+	return (
+		<>
+			<AppBar position='relative' style={{ padding: '20px 35px' }}>
+				<h2>Touch Typing Speed</h2>
+			</AppBar>
+			<Container style={{ marginTop: '20px' }} maxWidth='xl'>
+				{isCompleted ? (
+					<ModalWindow />
+				) : (
+					<Grid spacing={4} container>
+						<Grid item xs={9}>
+							<TextTyping
+								paperStyle={paperStyle}
+								setIsCompleted={setIsCompleted}
+							/>
+						</Grid>
+						<Grid
+							style={{ justifyContent: 'center', minWidth: '240px' }}
+							item
+							xs={3}
+						>
+							<Paper style={(paperStyle, { flexDirection: 'row' })}>
+								<ResultPanel />
+							</Paper>
+						</Grid>
+					</Grid>
+				)}
+			</Container>
+		</>
+	);
 };
 
 const paperStyle = {
-    padding: '20px',
-    display: 'flex',
-    justifyContent: 'center',
-    textAlign: 'justify',
+	padding: '20px',
+	display: 'flex',
+	justifyContent: 'center',
+	textAlign: 'justify',
 };
 
-const mapStateToProps = (state) => ({
-    loading: state.loading,
-    data: state.data,
+const mapStateToProps = state => ({
+	loading: state.loading,
+	data: state.data,
 });
 
 export default connect(mapStateToProps, { getData })(App);
